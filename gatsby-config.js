cddc0e20349a44acfc3669c2ebe5e49f7ff1c77b
Ondřej Hliba <hliba.ondra@live.com>
2018-07-18 22:57:04 +0200

    Add Sass Plugin To Site

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Site',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ],
 }
